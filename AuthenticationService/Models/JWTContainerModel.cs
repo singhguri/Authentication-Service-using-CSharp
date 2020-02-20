cc190dce8a8f2cce9c93afa2af9ccac56d2f31f6
root315 <indoswiss30@gmail.com>
2020-02-21 00:24:38 +0530

    Add project files.

---
+++ AuthenticationService/Models/JWTContainerModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationService.Models
+{
+    public class JWTContainerModel : IAuthContainerModel
+    {
+        #region Public Methods
+        public int ExpireMinutes { get; set; } = 10080;
+        public string SecretKey { get; set; } = "TW9zaGVFcmV6UHJpdmF0ZUtleQ==";
+        public string SecurityAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+
+        public Claim[] Claims { get; set; }
+        #endregion
+    }
+}
---
+++ AuthenticationService/Program.cs
@@ -0,0 +1,47 @@
+﻿using AuthenticationService.Managers;
+using AuthenticationService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAuthContainerModel model = GetJWTContainerModel("abc", "mmoshikoo@gmail.com");
+            IAuthService authService = new JWTService(model.SecretKey);
+
+            string token = authService.GenerateToken(model);
+
+            if (!authService.isTokenValid(token))
+                throw new UnauthorizedAccessException();
+            else
+            {
+                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
+            }
+        }
+
+        #region Private Methods
+        private static JWTContainerModel GetJWTContainerModel(string name, string email)
+        {
+            return new JWTContainerModel()
+            {
+                Claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, name),
+                    new Claim(ClaimTypes.Email, email)
+                }
+            };
+        }
+        #endregion
+
+    }
+}
